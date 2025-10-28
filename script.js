f85e96b540de898399c1a29bc363f96d127ed514
tx-w <27542400+tx-w@users.noreply.github.com>
2025-10-28 10:13:05 -0700

    info box created

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const allPaths = document.querySelectorAll('path');
+
+const infoBox = document.getElementById('info-box');
+
 const prefectureData = {
     "hokkaido": {
         name: "Hokkaido",
@@ -11,14 +15,21 @@ const prefectureData = {
     }
 }
 
-const allPaths = document.querySelectorAll('path');
-
 allPaths.forEach(path => {
     path.addEventListener('mouseover', (event) => {
+        const prefectureID = event.target.id;
+        const info = prefectureData[prefectureID];
+        
+        infoBox.innerHTML = `
+        <h3>${info.name}</h3>
+        <p>Capital: ${info.capital}</p>
+        <p>Population: $info{info.population}</p>
+        `;
 
+        infoBox.style.opacity = 1;
     });
-
+    
     path.addEventListener('mouseout', (event) => {
-
+        infoBox.style.opacity = 0;
     });
 });
